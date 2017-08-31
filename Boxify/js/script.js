2d395f38d539314db259d41dbaead2cf39c1f3b5
Emrah Ismayilzade <emrah.ismayilzade@mail.ru>
2017-08-31 05:57:33 +0400

    Boxify (not responsive)

---
+++ Boxify/js/script.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+
+    var team = $('.feature-slider');
+    team.owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: false,
+//        autoplaySpeed: 750,
+//        autoplayTimeout: 4000,
+//        autoplayHoverPause: true,
+        autoWidth: false,
+        nav: false,
+        dots: true,
+//        responsiveClass: true,
+//        responsive: {
+//            0: {
+//                items: 1,
+//                nav: true,
+//                dot: false,
+//                loop: true
+//            },
+//            600: {
+//                items: 1,
+//                nav: true,
+//                dot: false,
+//                loop: true
+//            },
+//            1000: {
+//                items: 1,
+//                nav: true,
+//                dot: false,
+//                loop: true
+//            }
+//        }
+    });
+});
+
+$(document).ready(function () {
+
+    var gallery = $('.gallery');
+    gallery.owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: false,
+//        autoplaySpeed: 750,
+//        autoplayTimeout: 4000,
+//        autoplayHoverPause: true,
+        autoWidth: false,
+        nav: false,
+        dots: true,
+//        responsiveClass: true,
+//        responsive: {
+//            0: {
+//                items: 1,
+//                nav: true,
+//                dot: false,
+//                loop: true
+//            },
+//            600: {
+//                items: 1,
+//                nav: true,
+//                dot: false,
+//                loop: true
+//            },
+//            1000: {
+//                items: 1,
+//                nav: true,
+//                dot: false,
+//                loop: true
+//            }
+//        }
+    });
+});
+
+
